f09d690b3e7646200998585953057aa65b598f5a
Garon <garoncdavis@gmail.com>
2025-02-12 09:31:17 -0500

    first commit

---
+++ chefClaude/scrimbaState/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+﻿import chefImg from "../assets/chef-claude-icon.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={chefImg} alt="chef logo" className="chefimg" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
---
+++ practice/index.js
@@ -0,0 +1,39 @@
+﻿/*
+Challenge 2:
+Given an array of strings, return an array where 
+the first letter of each string is capitalized
+*/
+
+const names = ["alice", "bob", "charlie", "danielle"];
+// -->        ["Alice", "Bob", "Charlie", "Danielle"]
+// Your code here
+
+// y = names.map(function (x) {
+//   firstLetter = x.charAt(0);
+//   flCap = firstLetter.toUpperCase();
+//   remaining = x.slice(1);
+//   capWord = flCap + remaining;
+//   return capWord;
+// });
+
+y = names.map((x) => {
+  return x[0].toUpperCase() + x.slice(1);
+});
+// console.log(y);
+
+/*
+Challenge 3:
+Given an array of strings, return an array of strings that wraps each
+of the original strings in an HTML-like <p></p> tag.
+
+E.g. given: ["Bulbasaur", "Charmander", "Squirtle"]
+return: ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+*/
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"];
+// -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+// Your code here
+
+y = pokemon.map((x) => `<p>${x}</p>`);
+
+console.log(y);
