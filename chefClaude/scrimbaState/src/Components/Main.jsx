d6b8e2fa66ea8fee15a016ec467d5b76b3ec1f27
garon <garoncdavis@gmail.com>
2025-02-12 17:48:46 -0500

    changing computers

--- chefClaude/scrimbaState/src/Components/Main.jsx
+++ chefClaude/scrimbaState/src/Components/Main.jsx
@@ -1,14 +1,54 @@
-﻿export default function Main() {
+﻿// export default function Main() {
+//   return (
+//     <main>
+//       <form className="add-ingredient-form">
+//         <input
+//           type="text"
+//           placeholder="e.g. oregano"
+//           aria-label="Add ingredient"
+//         />
+//         <button>Add ingredient</button>
+//       </form>
+//     </main>
+//   );
+// }
+export default function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+
+  const ingElement = ingredients.map((x) => <li key={x} className="ingredientLi">{x}</li>)
+  
+    /**
+     * Challenge:
+     * Add the new ingredient to the array of ingredients. Also, add a 
+     * console.log(ingredients) after adding the ingredient, because 
+     * **warning**: you aren't going to see the page update!
+     * 
+     * Hint: this is a one-liner solution, so don't overthink it 😅
+     */
+
+    function handleSubmit(event) {
+      event.preventDefault()
+      const formData = new FormData(event.currentTarget)
+      const newIngredient = formData.get("ingredient")
+      ingredients.push(newIngredient)
+      console.log(ingredients)
+      
+  }
+  
   return (
-    <main>
-      <form className="add-ingredient-form">
-        <input
-          type="text"
-          placeholder="e.g. oregano"
-          aria-label="Add ingredient"
-        />
-        <button>Add ingredient</button>
-      </form>
-    </main>
-  );
+      <main>
+          <form onSubmit={handleSubmit} className="add-ingredient-form">
+              <input 
+                  type="text"
+                  placeholder="e.g. oregano"
+                  aria-label="Add ingredient"
+                  name="ingredient"
+              />
+              <button>Add ingredient</button>
+          </form>
+          <ul>
+              {ingElement}
+          </ul>
+      </main>
+  )
 }
